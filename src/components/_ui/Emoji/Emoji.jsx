5ad0ba229c669d99298c686f70279b7abedd7214
Marguerite Roth <marguerite.roth@gmail.com>
2019-09-02 17:50:30 -0400

    Add Emoji component

---
+++ src/components/_ui/Emoji/Emoji.jsx
@@ -0,0 +1,34 @@
+// Emoji component by Amelia Wattenberger | github.com/wattenberger | @wattenberger
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import emojiMap from './emojiMap.jsx';
+
+import './Emoji.scss';
+
+const Emoji = ({ name, className, ...props }) => {
+    const emojiUrl = emojiMap[name]
+
+    return (
+        <img
+            className={[
+                "Emoji",
+                className
+            ].join(" ")}
+            src={emojiUrl}
+            {...props}
+        />
+    );
+};
+
+Emoji.propTypes = {
+    name: PropTypes.oneOf([
+        ...Object.keys(emojiMap),
+    ]),
+};
+
+Emoji.defaultProps = {
+};
+
+export default Emoji;
