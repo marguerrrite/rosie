1d6a9a57fe8474eb607362088349d63db5c8f823
Marguerite Roth <marguerite.roth@gmail.com>
2019-09-19 10:08:22 -0400

    Logo — add hover, remove emotion

--- src/components/_ui/Logo/Logo.jsx
+++ src/components/_ui/Logo/Logo.jsx
@@ -1,33 +1,15 @@
 import React from "react";
 import classNames from 'classnames';
-import styled from "@emotion/styled";
-import colors from "styles/colors";
-
-const LogoPlate = styled("div")`
-    background: ${colors.blue500};
-    width: 60px;
-    height: 51px;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    padding: 9px 12px;
-`
-
-const LogoType = styled("div")`
-    font-size: 18px;
-    font-weight: 800;
-    color: white;
-    text-decoration: none;
-    line-height: 1;
-`
+import "./Logo.scss";
 
 const Logo = ({className}) => (
-    <LogoPlate className={classNames("Logo", className)}>
-        <LogoType className="LogoType">
+    <div className={classNames("Logo", className)}>
+        <span className="Logo__dot Logo__dot--yellow"></span>
+        <span className="Logo__dot Logo__dot--magenta"></span>
+        <div className="Logo__type">
             M
-        </LogoType>
-    </LogoPlate>
+        </div>
+    </div>
 );
 
 export default Logo;
