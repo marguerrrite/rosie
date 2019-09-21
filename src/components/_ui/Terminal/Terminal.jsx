639dc5efd509d200ab249edc2e7cd3050c498296
Marguerite Roth <marguerite.roth@gmail.com>
2019-09-21 16:02:12 -0400

    Add Tools and Terminal components

---
+++ src/components/_ui/Terminal/Terminal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import MaxWidth from "components/_ui/MaxWidth/MaxWidth";
+import "./Terminal.scss";
+
+const Terminal = ({children, className, size}) => {
+    return (
+        <MaxWidth size="s" className={classNames(className, "Terminal")}>
+            <span className="Terminal__root">
+                marguerites-mbp:~ marge$
+            </span>
+        </MaxWidth>
+    );
+};
+
+export default Terminal;
