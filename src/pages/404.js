c7e321e0aae6b29ef8b21e34d2c5fd47bd43aa20
Marguerite Roth <marguerite.roth@gmail.com>
2019-10-20 11:42:36 -0400

    Update 404 component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Layout from "components/Layout/Layout";
+import MaxWidth from "components/_ui/MaxWidth/MaxWidth";
+import Link from "components/_ui/Link/Link";
+import Spinner from "components/_ui/Spinner/Spinner";
+import "./404.scss";
 
 const NotFoundPage = () => (
     <Layout>
-        <h1>
-            NOT FOUND
-        </h1>
-        <p>
-            You just hit a route that doesn&#39;t exist... the sadness.
-        </p>
+        <MaxWidth className="NotFound">
+            <Spinner
+                className="NotFound__spinner__container"
+                pet="oscar"
+                size="xl"
+            />
+            <h1 className="NotFound__header">
+                Page not found
+            </h1>
+            <Link to="/work"
+                className="NotFound__button"
+                isButton
+                buttonProps={{category: "secondary", size: "large"}}
+            >
+                Check out projects
+            </Link>
+        </MaxWidth>
+
     </Layout>
 )
 
