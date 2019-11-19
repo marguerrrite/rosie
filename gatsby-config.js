4656770c65de35b613e9d4e58f22680446796e45
Marguerite Roth <marguerite.roth@gmail.com>
2019-11-19 10:44:33 -0500

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -154,6 +154,7 @@ module.exports = {
         },
     },
     `gatsby-plugin-lodash`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
