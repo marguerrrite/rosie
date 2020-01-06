c367139fe2829cdb9738efa975d4db0ce2b2bf3d
Marguerite Roth <marguerite.roth@gmail.com>
2020-01-05 19:56:57 -0500

    ContestantHistogram: Add age difference, update axes

--- src/components/Bachelor/ContestantHistogram.jsx
+++ src/components/Bachelor/ContestantHistogram.jsx
@@ -3,24 +3,22 @@ import PropTypes from "prop-types"
 import classNames from 'classnames'
 import * as d3 from "d3"
 import _ from "lodash"
-
 import Chart from "components/Bachelor/Chart/Chart"
 import Bars from "components/Bachelor/Chart/Bars"
 import Axis from "components/Bachelor/Chart/Axis"
 import ContestantHead from "components/Bachelor/ContestantHead"
-import { useChartDimensions, accessorPropsType, useUniqueId } from "components/utils"
+import { useChartDimensions, accessorPropsType } from "components/utils"
 import peterCrownImg from './images/head-peter-crown.png'
 import "./ContestantHistogram.scss"
 
 const gradientColors = ["#9980FA", "rgb(226, 222, 243)"]
 
-const ContestantHistogram = ({ className, data, xAccessor, label, contestantAge, contestantName }) => {
-    const gradientId = useUniqueId("ContestantHistogram-gradient")
+const ContestantHistogram = ({ className, data, xAccessor, contestantAge, contestantName }) => {
     const [ref, dimensions] = useChartDimensions({
         marginBottom: 60,
         marginRight: 24,
         marginLeft: 0,
-        marginTop: 50
+        marginTop: 66
     })
 
     const numberOfThresholds = 9
@@ -54,17 +52,20 @@ const ContestantHistogram = ({ className, data, xAccessor, label, contestantAge,
     const ageAccessor = d => d.age
     const mean = d3.mean(data.filter(d => d.occupation), ageAccessor)
 
+    let ageDifference = 28 - contestantAge
+
+    console.log(ageDifference)
+
     return (
         <div className={classNames(className, "ContestantHistogram")} ref={ref}>
-            {/* <h6 className="ContestantHistogram__title">
+            <h5 className="ContestantHistogram__title">
                 Contestant Age Distribution
-            </h6> */}
+            </h5>
             <Chart dimensions={dimensions}>
                 <Axis
                     dimensions={dimensions}
                     dimension="x"
                     scale={xScale}
-                    label={label}
                 />
                 {/* <Axis
                     dimensions={dimensions}
@@ -81,31 +82,62 @@ const ContestantHistogram = ({ className, data, xAccessor, label, contestantAge,
                     heightAccessor={heightAccessorScaled}
                     contestantAge={contestantAge}
                 />
-                {contestantName && (
-                    <ContestantHead
-                        isSvg
-                        showNotch
-                        className="ContestantHistogram__head"
-                        name={contestantName}
-                        x={(xScale(contestantAge)) - 5}
-                        y={dimensions.boundedHeight + 20}
-                    />
-                )}
-                {contestantAge != 28 && (
+
+                {/* Peter Head */}
+                {/* {contestantAge != 28 && (
                     <svg
-                        x={(xScale(28)) + 11}
+                        x={(xScale(28))}
                         y={dimensions.boundedHeight + 20} >
                         <path d="M4.5 0L9 4H0L4.5 0Z" fill="#5892f2" />
                     </svg>
-                )}
+                )} */}
                 <image
                     className="ContestantHistogram__peter-head"
-                    x={(xScale(28)) - 3}
-                    y={contestantAge != 28 ? dimensions.boundedHeight + 23 : dimensions.boundedHeight + 64}
+                    x={contestantAge == 28 ? (xScale(28)) + 8 : (xScale(28))}
+                    y={dimensions.boundedHeight + 16}
                     width="36"
                     height="36"
                     href={peterCrownImg}
                 />
+
+                {contestantName && (
+                    <ContestantHead
+                        isSvg
+                        className="ContestantHistogram__head"
+                        name={contestantName}
+                        x={contestantAge == 28 ? (xScale(contestantAge)) - 8 : (xScale(contestantAge))}
+                        y={dimensions.boundedHeight + 16}
+                    />
+                )}
+
+                {/* todo: break this bit out to better handle phrasing */}
+                {ageDifference === 0 ? (
+                    <text
+                        className="ContestantHistogram__age__label"
+                        x={dimensions.width / 2}
+                        y={dimensions.boundedHeight + 70} >
+                        {contestantName} and Peter are the same age!
+                    </text>
+                ) : (
+                    <>
+                        {ageDifference > 0 ? (
+                            <text
+                            className = "ContestantHistogram__age__label"
+                            x = {dimensions.width / 2}
+                            y={dimensions.boundedHeight + 70} >
+                                Peter is {ageDifference} year{Math.abs(ageDifference) == 1 ? "" : <tspan>s</tspan>} older than {contestantName}
+                            </text>
+                        ) : (
+                            <text
+                                className="ContestantHistogram__age__label"
+                                x={dimensions.width / 2}
+                                y={dimensions.boundedHeight + 70} >
+                                {contestantName} is {Math.abs(ageDifference)} year{Math.abs(ageDifference) == 1 ? "" : <tspan>s</tspan>} older than Peter
+                            </text>
+                        )}
+                    </>
+                )}
+
                 <line
                     className="ContestantHistogram__mean"
                     x1={xScale(mean)}
@@ -116,16 +148,9 @@ const ContestantHistogram = ({ className, data, xAccessor, label, contestantAge,
                 <text
                     className="ContestantHistogram__mean__label"
                     x={xScale(mean)}
-                    y={-37}
-                >
-                    Average age:
-                </text>
-                <text
-                    className="ContestantHistogram__mean__age"
-                    x={xScale(mean)}
                     y={-22}
                 >
-                    {mean}
+                    Average age: {mean}
                 </text>
             </Chart>
         </div>
