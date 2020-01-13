dfe0738c95b53901e26569846afef6c5f3275980
Marguerite Roth <marguerite.roth@gmail.com>
2020-01-13 17:51:20 -0500

    Update index with recent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Link from "components/_ui/Link/Link"
+import ContestantHead from "components/Bachelor/ContestantHead"
 import Intro from "components/Intro/Intro"
 import Layout from "components/Layout/Layout"
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth";
@@ -12,6 +14,23 @@ const Home = () => (
     <Layout>
         <SEO />
         <Intro />
+        <MaxWidth className="Home__recent">
+            <h1>
+                Recent Project
+            </h1>
+            <Link to="/2020-bachelor" className="Home__recent__project">
+                <ContestantHead name="Deandra" />
+                <div>
+                    <h3>
+                        Bachelor 2020 Contestant Guide
+                    </h3>
+                    <p>
+                        Updating weekly with episode information and improved components.
+                    </p>
+                </div>
+
+            </Link>
+        </MaxWidth>
         <MaxWidth id="work" className="Home__projects">
             <h1>
                 Selected Work
