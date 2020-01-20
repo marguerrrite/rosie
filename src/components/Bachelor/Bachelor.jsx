ed77f6242084b672d9dc590225f943b272c2d9a8
Marguerite Roth <marguerite.roth@gmail.com>
2020-01-20 18:03:09 -0500

    Bachelor.jsx: temporarily comment out all components (other than table)

--- src/components/Bachelor/Bachelor.jsx
+++ src/components/Bachelor/Bachelor.jsx
@@ -1,29 +1,98 @@
 import React, { useState, useEffect } from "react"
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import Contestants from 'components/Bachelor/Contestants'
-import Link from "components/_ui/Link/Link"
+import ContestantTable from 'components/Bachelor/ContestantTable'
 import MaxWidth from 'components/_ui/MaxWidth/MaxWidth'
-import Label from "components/_ui/Label/Label";
-//import peterCrownImg from './images/head-peter-crown.png'
 import './Bachelor.scss'
+// import contestantsData from 'components/Bachelor/data/parsedContestants.csv'
 
-import contestantsDataA from 'components/Bachelor/data/bachelor-cosmo.csv'
-import contestantsDataB from 'components/Bachelor/data/bachelor-usmag.csv'
-import contestantsDataC from 'components/Bachelor/data/bachelor-abc.csv'
+// import contestantsDataA from 'components/Bachelor/data/bachelor-cosmo.csv'
+// import contestantsDataB from 'components/Bachelor/data/bachelor-usmag.csv'
+// import contestantsDataC from 'components/Bachelor/data/bachelor-abc.csv'
 
 const Bachelor = () => {
-    const [parsedContestants, setParsedContestants] = useState('')
+    // const [parsedContestants, setParsedContestants] = useState('')
 
-    useEffect(() => {
-        // You need to restrict it at some point
-        // This is just dummy code and should be replaced by actual
-        if (!parsedContestants) {
-            getParsedContestants();
-        }
-    }, []);
+    // useEffect(() => {
+    //     // You need to restrict it at some point
+    //     // This is just dummy code and should be replaced by actual
+    //     if (!parsedContestants) {
+    //         setParsedContestants(contestantsData);
+    //     }
+    // }, []);
 
-    // const getParsedContestants = async () => {
+    return (
+        <MaxWidth size="l" className="Bachelor">
+            {/* <MaxWidth size="l" className="Bachelor__header">
+                <h1 className="Bachelor__header__title">
+                    The Bachelor
+                </h1>
+                <h3 className="Bachelor__header__description">
+                    Welcome Bachelor Nation. Following along with the new contestants is hard—this guide is here to help! 🌹
+                </h3>
+            </MaxWidth> */}
+
+            {/* <MaxWidth size="l" className="Bachelor__contestants">
+                <Contestants parsedContestants={parsedContestants} />
+            </MaxWidth> */}
+
+            <MaxWidth size="l" className="Bachelor__table">
+                <ContestantTable />
+            </MaxWidth>
+
+            {/* <MaxWidth size="l" className="Bachelor__inspo">
+                <h6>
+                    <Link doOpenInNewTab to="https://robhasawebsite.com/shows/reality-tv-rhapups/bachelor-bachelorette-paradise/bachelor-2/">
+                        Inspired by the Bachelor RHAP UP podcast 🌹
+                    </Link>
+                </h6>
+            </MaxWidth>
+
+            <MaxWidth size="xl" className="Bachelor__back-button__container">
+                <Link to="/" isButton className="Bachelor__back-button">
+                    ← Back to Marguerite.io
+                </Link>
+            </MaxWidth>
+
+            <MaxWidth size="xl" className="Bachelor__inspo">
+                <Link doOpenInNewTab to="https://github.com/margueriteroth/rosie/tree/master/src/components/Bachelor" className="Bachelor__code">
+                    Bachelor code
+                </Link>
+            </MaxWidth>
+
+            <MaxWidth size="xl" className="Bachelor__attributions">
+                <h5 className="Bachelor__attributions__title">
+                    Contestant image, name, age, occupation, and location information from:
+                </h5>
+                <p>
+                    Us Magazine: <Link to="https://www.usmagazine.com/entertainment/pictures/the-bachelor-season-24-cast-meet-peter-webers-contestants">
+                        https://www.usmagazine.com/entertainment/pictures/the-bachelor-season-24-cast-meet-peter-webers-contestants
+                    </Link>
+                </p>
+                <p>
+                    Cosmopolitan: <Link to="https://www.cosmopolitan.com/entertainment/tv/g29087999/peter-weber-bachelor-contestants-season-24-2020/">
+                        https://www.cosmopolitan.com/entertainment/tv/g29087999/peter-weber-bachelor-contestants-season-24-2020/
+                    </Link>
+                </p>
+                <p>
+                    ABC: <Link to="https://abc.com/shows/the-bachelor">
+                        https://abc.com/shows/the-bachelor
+                    </Link>
+                </p>
+            </MaxWidth> */}
+        </MaxWidth>
+    )
+}
+
+export default Bachelor;
+
+
+
+
+
+
+
+
+// const getParsedContestants = async () => {
     //     const contestantsData = await Promise.all(
     //         contestantsDataA.map(async (d, i) => {
     //             let location = d.location
@@ -54,97 +123,35 @@ const Bachelor = () => {
     //     setParsedContestants(contestantsData);
     // };
 
-    const getParsedContestants = () => {
-        const contestantsData = contestantsDataA.map((d, i) => {
-            let name = contestantsDataB[i].name
-            let image = contestantsDataB[i]["image-src"]
-            let lat = d.lat
-            let lng = d.lng
-            let bio = contestantsDataC[i].bio
-            let notes = contestantsDataC[i].notes
 
-            return {
-                ...d,
-                lat,
-                lng,
-                name,
-                image,
-                bio,
-                notes
-            }
-        })
 
-        setParsedContestants(contestantsData);
-    };
+// useEffect(() => {
+//     // You need to restrict it at some point
+//     // This is just dummy code and should be replaced by actual
+//     if (!parsedContestants) {
+//         getParsedContestants();
+//     }
+// }, []);
 
-    return (
-        <MaxWidth size="l" className="Bachelor">
-            <MaxWidth size="l" className="Bachelor__header">
-                <h1 className="Bachelor__header__title">
-                    The Bachelor
-                </h1>
-                <h3 className="Bachelor__header__description">
-                    Welcome Bachelor Nation. Following along with the new contestants is hard—this guide is here to help! 🌹
-                </h3>
-                {/* <img className="Bachelor__header__peter" src={peterCrownImg} />
-                <h4 className="Bachelor__header__name">
-                    Peter Weber, 28
-                </h4>
-                <div className="Bachelor__header__description">
-                    <span>
-                        <FontAwesomeIcon className="" icon={faMapMarkerAlt} /> Westlake Village, CA
-                    </span>
-                    <span>
-                        ✈️ Airline Pilot
-                    </span>
-                    <span>
-                        🌹Hannah’s Season
-                    </span>
-                </div> */}
-            </MaxWidth>
-            <MaxWidth size="l" className="Bachelor__contestants">
-                <Contestants parsedContestants={parsedContestants} />
-            </MaxWidth>
-            <MaxWidth size="l" className="Bachelor__inspo">
-                <h6>
-                    <Link doOpenInNewTab to="https://robhasawebsite.com/shows/reality-tv-rhapups/bachelor-bachelorette-paradise/bachelor-2/">
-                        Inspired by the Bachelor RHAP UP podcast 🌹
-                    </Link>
-                </h6>
-            </MaxWidth>
-            <MaxWidth size="xl" className="Bachelor__back-button__container">
-                <Link to="/" isButton className="Bachelor__back-button">
-                    ← Back to Marguerite.io
-                </Link>
-            </MaxWidth>
-            <MaxWidth size="xl" className="Bachelor__inspo">
-                <Link doOpenInNewTab to="https://github.com/margueriteroth/rosie/tree/master/src/components/Bachelor" className="Bachelor__code">
-                    Bachelor code
-                </Link>
-            </MaxWidth>
-            <MaxWidth size="xl" className="Bachelor__attributions">
-                <h5 className="Bachelor__attributions__title">
-                    Contestant image, name, age, occupation, and location information from:
-                </h5>
-                <p>
-                    Us Magazine: <Link to="https://www.usmagazine.com/entertainment/pictures/the-bachelor-season-24-cast-meet-peter-webers-contestants">
-                        https://www.usmagazine.com/entertainment/pictures/the-bachelor-season-24-cast-meet-peter-webers-contestants
-                    </Link>
-                </p>
-                <p>
-                    Cosmopolitan: <Link to="https://www.cosmopolitan.com/entertainment/tv/g29087999/peter-weber-bachelor-contestants-season-24-2020/">
-                        https://www.cosmopolitan.com/entertainment/tv/g29087999/peter-weber-bachelor-contestants-season-24-2020/
-                    </Link>
-                </p>
-                <p>
-                    ABC: <Link to="https://abc.com/shows/the-bachelor">
-                        https://abc.com/shows/the-bachelor
-                    </Link>
-                </p>
-            </MaxWidth>
-        </MaxWidth>
-    )
-}
+// const getParsedContestants = () => {
+//     const contestantsData = contestantsDataA.map((d, i) => {
+//         let name = contestantsDataB[i].name
+//         let image = contestantsDataB[i]["image-src"]
+//         let lat = d.lat
+//         let lng = d.lng
+//         let bio = contestantsDataC[i].bio
+//         let notes = contestantsDataC[i].notes
 
-export default Bachelor;
+//         return {
+//             ...d,
+//             lat,
+//             lng,
+//             name,
+//             image,
+//             bio,
+//             notes
+//         }
+//     })
 
+//     setParsedContestants(contestantsData);
+// };
