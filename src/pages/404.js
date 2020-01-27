54b834234c94f1dd74f8219e5f780f72e936b5ad
Marguerite Roth <marguerite.roth@gmail.com>
2020-01-27 10:39:51 -0500

    update 404 page layout and add recents

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ContestantHead from "components/Bachelor/ContestantHead"
+import Label from "components/_ui/Label/Label";
 import Layout from "components/Layout/Layout";
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth";
 import Link from "components/_ui/Link/Link";
@@ -16,15 +18,43 @@ const NotFoundPage = () => (
             <h1 className="NotFound__header">
                 Page not found
             </h1>
-            <Link to="/#work"
-                className="NotFound__button"
-                isButton
-                buttonProps={{category: "primary", size: "l"}}
-            >
-                Check out projects
-            </Link>
-        </MaxWidth>
+            <div className="NotFound__recent">
+                <div>
+                    <Label>
+                        Check out my recent project
+                    </Label>
+                    <Link to="/2020-bachelor" className="NotFound__recent__item NotFound__recent__item--project">
+                        <ContestantHead name="Deandra" />
+                        <div>
+                            <h3>
+                                Bachelor 2020 Contestant Guide
+                            </h3>
+                            <p>
+                                Updating weekly with episode information and improved components. Wip!
+                            </p>
+                        </div>
 
+                    </Link>
+                </div>
+                <div>
+                    <Label>
+                        Check out my recent post
+                    </Label>
+                    <Link to="/blog/set-up-local-website"
+                        className="NotFound__recent__item NotFound__recent__item--post">
+                        <div>
+                            <h3>
+                                Set up a local dev environment and run a website
+                            </h3>
+                            <p>
+                                Collect and utilize the tools needed to spin up and edit a website on your computer.
+                                Basic HTML/CSS/JS, D3.js, and Twitter Bootstrap starters provided.
+                            </p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </MaxWidth>
     </Layout>
 )
 
